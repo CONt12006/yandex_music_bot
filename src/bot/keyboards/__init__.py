7094c2f0bb2e5074f1b3e8544715e5cd357ec715
CONt12006 <162928166+CONt12006@users.noreply.github.com>
2026-04-07 12:15:39 +0300

    Create __init__.py

---
+++ src/bot/keyboards/__init__.py
@@ -0,0 +1,13 @@
+from .main_menu import (
+    get_main_menu_keyboard, 
+    get_back_button, 
+    get_auth_keyboard,   
+    get_playlists_keyboard  
+)
+
+__all__ = [
+    'get_main_menu_keyboard', 
+    'get_back_button', 
+    'get_auth_keyboard',      
+    'get_playlists_keyboard'  
+]
